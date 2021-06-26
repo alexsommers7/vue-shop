e0a1b25b7bdac5668282318d9aab2838dd1c0f0b
alexsommers7 <asommers22@hotmail.com>
2021-06-26 12:38:43 -0700

    adding aria-label to sort input

--- src/components/SortProducts.vue
+++ src/components/SortProducts.vue
@@ -2,7 +2,7 @@
   <div class="columns small-6 large-1 nav__sort">
     <p>Sort by:</p>
     <form name="sort__form" action="">
-      <select class="sort__select" name="sort__select" @change="onProductSort">
+      <select class="sort__select" name="sort__select" @change="onProductSort" aria-label="Sort Products">
         <option value="" selected="" disabled="" hidden="">Choose here</option>
         <option value="0">Price, Low to High</option>
         <option value="1">Price, High to Low</option>
