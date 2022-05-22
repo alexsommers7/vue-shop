46d71c58786b7d591a5700cb846614fbba228fff
alexsommers7 <asommers22@hotmail.com>
2022-05-21 17:02:30 -0700

    pinia init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import Vue from "vue";
-import App from "./App";
+import Vue from 'vue';
+import App from './App';
+import './utils/filters';
+import { createPinia, PiniaVuePlugin } from 'pinia';
+
+Vue.use(PiniaVuePlugin);
+const pinia = createPinia();
 
 new Vue({
   render: (h) => h(App),
-}).$mount("#app");
+  pinia,
+}).$mount('#app');
