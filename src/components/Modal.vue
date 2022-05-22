33d5ab72e65cd0d17cb8bda83798b6eac106012b
alexsommers7 <asommers22@hotmail.com>
2022-05-21 17:04:01 -0700

    create modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="modal" :class="{ active: modalOpen }" @click.self="modalOpen = false">
+    <div class="modal__content">
+      <button class="modal__close" @click.self="modalOpen = false">x</button>
+      <div class="modal__description">
+        <img class="modal__image" :src="modalData.selectedImagePath" :alt="modalData.selectedImageAlt" />
+        <div class="modal__text-content">
+          <div class="modal__title__price">
+            <p class="modal__title">
+              <strong>{{ modalData.selectedTitle }}</strong>
+            </p>
+            <p class="modal__price">{{ modalData.selectedPrice | formatUSPrice }}</p>
+          </div>
+          <p class="modal__text">{{ modalData.selectedDescription }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  data() {
+    return {
+      modalOpen: false,
+    };
+  },
+  props: {
+    modalData: Object,
+  },
+  watch: {
+    modalData: {
+      handler: function(newObj) {
+        if (newObj.openTriggered && !this.modalOpen) this.modalOpen = true;
+      },
+      deep: true,
+    },
+    modalOpen: {
+      handler: function() {
+        this.modalOpen ? document.body.classList.add('no-scroll') : document.body.classList.remove('no-scroll');
+      },
+    },
+  },
+};
+</script>
