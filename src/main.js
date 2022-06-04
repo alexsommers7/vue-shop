1a144641105c2a05f17c0e50a7415b80299227c9
alexsommers7 <asommers22@hotmail.com>
2022-06-04 14:05:38 -0700

    Init Quatar

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import { Quasar } from 'quasar';
+import quasarUserOptions from './quasar-user-options';
 
 const app = createApp({
   ...App,
 });
 
+app.use(Quasar, quasarUserOptions);
 app.use(createPinia());
 app.mount('#app');
