38bdefc4bd7b8879d722f02b9345206bc04a1544
alexsommers7 <asommers22@hotmail.com>
2022-07-30 09:29:33 -0700

    Update layout to extend drawer over header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="hHh lpR fFf" class="bg-grey-1">
+  <q-layout view="lHh lpR fFf" class="bg-grey-1">
     <LayoutHeader></LayoutHeader>
 
     <ProductFilter></ProductFilter>
